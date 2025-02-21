cc656ed39fa5ecee491b102f9401280f08bb3594
candyfloss1534 <vanlong1534@gmail.com>
2025-02-21 10:18:11 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+ let u = username.value
+ let p = pw.value
+ if (u=="anh" && p=="12345")
+ {
+  window.open("tênlink???.html")
+  document.getElementById(compiler).style.display="block"
+ }
+}
